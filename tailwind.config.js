121f622a6801b5ff86097016192e49524005ba71
redaezziani <klausdev2@gmail.com>
2024-09-22 13:37:51 +0100

    test message

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,28 @@ export default {
 	theme: {
         extend: {
             colors:{
-                primary:'#115e59',
+                primary:'#a52a2a',
                 secondary:'#fcd34d',
             }
             ,
             animation: {
               gradient: "gradient 8s linear infinite",
+              border: 'border 4s linear infinite',
+              'infinite-scroll': 'infinite-scroll 25s linear infinite',
             },
             keyframes: {
               gradient: {
                 to: {
                   backgroundPosition: "var(--bg-size) 0",
                 },
+              },
+
+              border: {
+                to: { '--border-angle': '360deg' },
+              },
+              'infinite-scroll': {
+                from: { transform: 'translateX(0)' },
+                to: { transform: 'translateX(-100%)' },
               },
             },
           },
